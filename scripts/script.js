dab5937bf95eb9174a05ed94be417bb516df76bc
Martin Kruger <martin@martinkruger.com>
2020-09-02 15:00:01 +0200

    New header

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,37 @@
-console.log("test")
-
+// Going to minus header from hero
+const HERO_SIZE = 300;
+const HEADER_SIZE = 58;
 
 const header = document.getElementById('header-fx');
 
-setTimeout(() => header.classList.add('header-fx'), 100)
-
+setTimeout(() => header.classList.add('header-fx'), 100);
 
 function openNav() {
-  document.getElementById("myNav").classList.remove("nav-hidden-mobile");
-  document.getElementById("openNav").style.display="none";
+  document.getElementById('myNav').classList.remove('nav-hidden-mobile');
+  document.getElementById('openNav').style.display = 'none';
 }
 
 function closeNav() {
-  document.getElementById("myNav").classList.add("nav-hidden-mobile");
-  document.getElementById("openNav").style.display="inline-block";
+  document.getElementById('myNav').classList.add('nav-hidden-mobile');
+  document.getElementById('openNav').style.display = 'inline-block';
 }
-
-
 
 window.onscroll = function () {
-  changeTitleClass()
-}
+  changeTitleClass();
+};
 
-var title = document.getElementById('myTitle')
+var title = document.getElementById('myTitle');
 
-var sticky = title.offsetTop
+var sticky = title.offsetTop;
 
 function changeTitleClass() {
-  if (window.pageYOffset > '5') {
-    title.classList.add('title-background')
+  if (window.pageYOffset > HERO_SIZE - HEADER_SIZE) {
+    title.classList.add('title-background');
   } else {
-    title.classList.remove('title-background')
+    title.classList.remove('title-background');
   }
 }
 
 function myFunction() {
-  alert("The form was submitted");
+  alert('The form was submitted');
 }
